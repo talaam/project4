db13b42938fc831d3c09bab5b2f2ddb50026fe0d
EC2 Default User <ec2-user@ip-172-31-68-24.ec2.internal>
2023-03-05 11:48:10 +0000

    project upload

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=obadiahtalaam/udacityproject3
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run udacityproject3\
+    --image=$dockerpath\
+    --port=80 --labels app=udacityproject3
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udacityproject3 8000:80
+
